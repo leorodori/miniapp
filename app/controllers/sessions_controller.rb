c1786aa5098ab4b8aa797adccd27601ec38c9b15
Leio <leo.2001.19@icloud.com>
2026-09-10 00:08:13 +0900

    feat: ログイン機能のエラーを解消し正常動作を確認

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
 
     if user&.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to words_index_path, notice: "ログインしました"
+      redirect_to words_path, notice: "ログインしました"
     else
       flash.now[:alert] = "メールアドレスまたはパスワードが違います"
       render :new, status: :unprocessable_entity
